7b278b26a1da720f887f2ec23171ad42c4898dc8
Derek Wong <derek.wong@itapps.com>
2023-11-16 14:33:38 +0800

    1980. Find unique binary string

---
+++ Leetcode/Medium/Find-unique-binary-string/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        List<Integer> intList = new ArrayList<Integer>();
+
+        for(String s : nums){
+            intList.add(Integer.parseInt(s, 2));
+        }
+
+        double max = Math.pow((double) 2, (double) n);
+
+        for(int i = (int) max - 1; i >= 0; i--){
+            if(intList.contains(i)) continue;
+
+            String result = Integer.toBinaryString(i);
+
+            if(result.length() != n){
+                int len = n - result.length();
+                String s = String.format("%0" + len + 'd', 0);
+                result = s + result;
+            }
+
+            return result;
+        }
+
+        return "";
+        
+    }
+}
